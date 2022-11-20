7d3320af378d17f90aecc9382c43d2c962723f41
Titus Wormer <tituswormer@gmail.com>
2022-11-20 11:49:51 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {
   clean,
   trigrams,
@@ -7,110 +8,109 @@ import {
   tuplesAsDictionary
 } from './index.js'
 
-test('.clean', function (t) {
+test('.clean', function () {
   const ignore = '!"#$%&\'()*+,-./0123456789:;<=>?@'
   let index = -1
 
-  t.equal(typeof clean('test'), 'string', 'should return a string')
-  t.equal(clean(), '', 'should accept a missing value')
-  t.equal(clean(null), '', 'should accept `null`')
-  t.equal(clean(undefined), '', 'should accept `undefined`')
+  assert.equal(typeof clean('test'), 'string', 'should return a string')
+  assert.equal(clean(), '', 'should accept a missing value')
+  assert.equal(clean(null), '', 'should accept `null`')
+  assert.equal(clean(undefined), '', 'should accept `undefined`')
 
   while (++index < ignore.length) {
-    t.equal(
+    assert.equal(
       clean(ignore.charAt(index)),
       '',
       'should remove `' + ignore.charAt(index) + '`'
     )
   }
 
-  t.equal(clean('a  b'), 'a b', 'should collapse multiple spaces to one')
-  t.equal(
+  assert.equal(clean('a  b'), 'a b', 'should collapse multiple spaces to one')
+  assert.equal(
     clean('a\n b'),
     'a b',
     'should collapse newlines and spaces to a space'
   )
-  t.equal(clean('a \tb'), 'a b', 'should collapse tabs and spaces to a space')
+  assert.equal(
+    clean('a \tb'),
+    'a b',
+    'should collapse tabs and spaces to a space'
+  )
 
-  t.equal(clean('\n\ta'), 'a', 'should trim initial white-space')
-  t.equal(clean('a \f'), 'a', 'should trim final white-space')
-  t.equal(clean('\ta \f'), 'a', 'should trim surrounding white-space')
-  t.equal(clean('\t\n \f'), '', 'should trim white-space only input')
+  assert.equal(clean('\n\ta'), 'a', 'should trim initial white-space')
+  assert.equal(clean('a \f'), 'a', 'should trim final white-space')
+  assert.equal(clean('\ta \f'), 'a', 'should trim surrounding white-space')
+  assert.equal(clean('\t\n \f'), '', 'should trim white-space only input')
 
-  t.equal(clean('AlPHA'), 'alpha', 'should lowercase mixed-case')
-  t.equal(clean('BRAVO'), 'bravo', 'should lowercase uppercase')
-  t.equal(clean('Charlie'), 'charlie', 'should lowercase sentence-case')
-
-  t.end()
+  assert.equal(clean('AlPHA'), 'alpha', 'should lowercase mixed-case')
+  assert.equal(clean('BRAVO'), 'bravo', 'should lowercase uppercase')
+  assert.equal(clean('Charlie'), 'charlie', 'should lowercase sentence-case')
 })
 
-test('.trigrams', function (t) {
-  t.ok(Array.isArray(trigrams('test')), 'should return an array')
-  t.equal(
+test('.trigrams', function () {
+  assert.ok(Array.isArray(trigrams('test')), 'should return an array')
+  assert.equal(
     trigrams('test').join(','),
     ' te,tes,est,st ',
     'should return trigrams'
   )
-  t.equal(
+  assert.equal(
     trigrams('te@st').join(','),
     ' te,te ,e s, st,st ',
     'should return cleaned trigrams (1)'
   )
-  t.equal(
+  assert.equal(
     trigrams('\nte\tst ').join(','),
     ' te,te ,e s, st,st ',
     'should return cleaned trigrams (2)'
   )
-  t.end()
 })
 
-test('.asDictionary', function (t) {
-  t.equal(typeof asDictionary('test'), 'object', 'should return an object')
+test('.asDictionary', function () {
+  assert.equal(typeof asDictionary('test'), 'object', 'should return an object')
 
-  t.deepEqual(
+  assert.deepEqual(
     asDictionary('test'),
     {'st ': 1, est: 1, tes: 1, ' te': 1},
     'should return trigrams'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     asDictionary('te@st'),
     {' st': 1, ' te': 1, 'e s': 1, 'st ': 1, 'te ': 1},
     'should return cleaned trigrams (1)'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     asDictionary('\nte\tst '),
     {' st': 1, ' te': 1, 'e s': 1, 'st ': 1, 'te ': 1},
     'should return cleaned trigrams (2)'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     asDictionary('testtest'),
     {' te': 1, est: 2, 'st ': 1, stt: 1, tes: 2, tte: 1},
     'should count duplicate trigrams'
   )
-
-  t.end()
 })
 
-test('.asTuples', function (t) {
-  t.ok(Array.isArray(asTuples('test')), 'should return an array')
-  t.equal(
+test('.asTuples', function () {
+  assert.ok(Array.isArray(asTuples('test')), 'should return an array')
+  assert.equal(
     asTuples('test')
       .map((d) => d.join(';'))
       .join(','),
     ' te;1,tes;1,est;1,st ;1',
     'should return tuples'
   )
-  t.equal(
+  assert.equal(
     asTuples('te@st')
       .map((d) => d.join(';'))
       .join(','),
     ' te;1,te ;1,e s;1, st;1,st ;1',
     'should return cleaned trigrams (1)'
   )
-  t.equal(
+  assert.equal(
     asTuples('\nte\tst ')
       .map((d) => d.join(';'))
       .join(','),
@@ -118,7 +118,7 @@ test('.asTuples', function (t) {
     'should return cleaned trigrams (2)'
   )
 
-  t.equal(
+  assert.equal(
     asTuples('testtest')
       .map((d) => d.join(';'))
       .join(','),
@@ -126,35 +126,31 @@ test('.asTuples', function (t) {
     'should count duplicate trigrams'
   )
 
-  t.equal(
+  assert.equal(
     asTuples('testtest')
       .map((d) => d.join(';'))
       .join(','),
     ' te;1,stt;1,tte;1,st ;1,tes;2,est;2',
     'should sort trigrams'
   )
-
-  t.end()
 })
 
-test('.tuplesAsDictionary', function (t) {
-  t.equal(
+test('.tuplesAsDictionary', function () {
+  assert.equal(
     typeof tuplesAsDictionary(asTuples('test')),
     'object',
     'should return an object'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     tuplesAsDictionary(asTuples('test')),
     {' te': 1, est: 1, 'st ': 1, tes: 1},
     'should return tuples as a dictionary'
   )
 
-  t.deepEqual(
+  assert.deepEqual(
     tuplesAsDictionary(asTuples('testtest')),
     {' te': 1, est: 2, 'st ': 1, stt: 1, tes: 2, tte: 1},
     'should count duplicate trigrams'
   )
-
-  t.end()
 })
